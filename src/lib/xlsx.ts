7f5609c8945123ae051e3b11928472c5c46037f5
Vikas Wakde <110342308+vikaswakde@users.noreply.github.com>
2024-05-29 07:58:22 +0530

    ignore error ts

--- src/lib/xlsx.ts
+++ src/lib/xlsx.ts
@@ -1,5 +1,5 @@
-import xlsx, { IJsonSheet } from "json-as-xlsx";
 import { people } from "@/people";
+import xlsx, { IJsonSheet } from "json-as-xlsx";
 
 export function downloadToExcel() {
   let columns: IJsonSheet[] = [
@@ -13,16 +13,26 @@ export function downloadToExcel() {
         { label: "Gender", value: "gender" },
         {
           label: "Date of Birth",
-          value: (row) => row.date_of_birth ? new Date(row.date_of_birth).toLocaleDateString() : "",
+          value: (row) => {
+            // @ts-ignore
+            const date_of_birth = row?.getValue("date_of_birth") ?? "";
+            const formatted = new Date(
+              date_of_birth as string
+            ).toLocaleDateString();
+            return formatted;
+          },
+          // ({ row }) => {
+          //   const date_of_birth = row.getValue("date_of_birth")
+          //   const formatted = new Date(date_of_birth as string).toLocaleDateString();
+          //   return formatted;
+          // },
         },
       ],
       content: people,
     },
   ];
-
   let settings = {
     fileName: "People Excel",
   };
-
   xlsx(columns, settings);
 }
